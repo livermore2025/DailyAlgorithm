220a9d9a5b94a6608cb37e0a983abb724d880522
gunbos1031 <69377496+gunbos1031@users.noreply.github.com>
2022-02-24 09:16:11 +0900

    to pull

--- getItemFromTwoSortedArrays.js
+++ getItemFromTwoSortedArrays.js
@@ -18,5 +18,12 @@ const getItemFromTwoSortedArrays = (arr1, arr2, k) => {
   // TODO: 여기에 코드를 작성합니다.
 };
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(binarySearch(arr, 5));
+const arr = [1, 2, 5, 8, 9, 10, 21, 23];
+console.log(binarySearch(arr, 7));
+
+let arr1 = [1, 4, 8, 10];
+let arr2 = [2, 3, 5, 9];
+let result = getItemFromTwoSortedArrays(arr1, arr2, 6);
+console.log(result); // --> 8
+
+// 서로 길이가 다른 자연수 배열의 전체 요소 중 k번째 요소를 리턴.
