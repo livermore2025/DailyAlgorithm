cf817f515e560130b5088a2f0512cdb6d3d09633
livermore2025 <livermore2025@gmail.com>
2022-03-24 07:58:06 +0900

    35 Ref

--- uglyNumberRef.js
+++ uglyNumberRef.js
@@ -1,6 +1,8 @@
-const uglyNumbers = function(n) {
+const uglyNumbers = function (n) {
   const uglyNum = [1];
-  let idx2 =0, idx3 = 0, idx5 = 0;
+  let idx2 = 0,
+    idx3 = 0,
+    idx5 = 0;
 
   for (let i = 0; i < n; i++) {
     const nextMultipleOf2 = uglyNum[idx2] * 2;
@@ -9,8 +11,8 @@ const uglyNumbers = function(n) {
     const nextUglyNum = Math.min(
       nextMultipleOf2,
       nextMultipleOf3,
-      nextMultipleOf5,
-    )
+      nextMultipleOf5
+    );
     uglyNum.push(nextUglyNum);
 
     if (nextUglyNum === nextMultipleOf2) idx2++;
@@ -18,8 +20,7 @@ const uglyNumbers = function(n) {
     if (nextUglyNum === nextMultipleOf5) idx5++;
   }
   return uglyNum[n - 1];
-}
+};
 
 let result = uglyNumbers(4);
 console.log(result); // --> 3
-
